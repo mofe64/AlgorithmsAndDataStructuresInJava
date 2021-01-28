d5d87908d4da6ad385070533951d2f0581e4a5e0
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-28 17:28:17 +0100

    Merge Sort

--- src/Algorithms/MergeSort.java
+++ src/Algorithms/MergeSort.java
@@ -2,26 +2,50 @@ package Algorithms;
 
 public class MergeSort {
 
-    public static void sortArray(int[] data, int lowestIndex, int highestIndex) {
+    public static void mergeSort(int[] data, int lowestIndex, int highestIndex) {
         if (lowestIndex < highestIndex) {
-            //get the index of the element in the middle
-            int middleIndex = lowestIndex + (highestIndex - lowestIndex) / 2;
-            int middleIndexPlusOne = middleIndex + 1;
-            //sort left side of array
-            sortArray(data, lowestIndex, middleIndex);
-            //sort right side of the array
-            sortArray(data, middleIndexPlusOne, highestIndex);
-            //combine both halves
-            merge(data, lowestIndex, middleIndex, middleIndexPlusOne, highestIndex);
+            int middleIndex = (lowestIndex + highestIndex) / 2;
+            mergeSort(data, lowestIndex, middleIndex);
+            mergeSort(data, middleIndex + 1, highestIndex);
+            merge(data, lowestIndex, middleIndex, highestIndex);
         }
 
     }
 
-    private static void merge(int[] data, int lowestIndex, int middleIndex, int middleIndexPlusOne, int highestIndex) {
-        int leftIndex = lowestIndex;
-        int rightIndex = middleIndexPlusOne;
-        int combinedIndex = lowestIndex;
-        int[] combined = new int[data.length];
+    private static void merge(int[] array, int lowestIndex, int middleIndex, int highestIndex) {
+        // Creating temporary sub Arrays
+        int[] leftArray = new int[middleIndex - lowestIndex + 1];
+        int[] rightArray = new int[highestIndex - middleIndex];
 
+        // Copying our sub Arrays into temporaries
+        for (int i = 0; i < leftArray.length; i++) {
+            leftArray[i] = array[lowestIndex + i];
+        }
+
+        for (int i = 0; i < rightArray.length; i++) {
+            rightArray[i] = array[middleIndex + i + 1];
+        }
+
+        // Iterators containing current index of temp sub Arrays
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        // Copying from leftArray and rightArray back into array
+        for (int i = lowestIndex; i < highestIndex + 1; i++) {
+            if (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+                if (leftArray[leftIndex] < rightArray[rightIndex]) {
+                    array[i] = leftArray[leftIndex];
+                    leftIndex++;
+                } else {
+                    array[i] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+            } else if (leftIndex < leftArray.length) {
+                array[i] = leftArray[leftIndex];
+            } else if (rightIndex < rightArray.length) {
+                array[i] = rightArray[rightIndex];
+            }
+        }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Algorithms.InsertionSort;
+import Algorithms.MergeSort;
 
 import java.security.SecureRandom;
 import java.util.Arrays;
@@ -11,8 +12,21 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             array[i] = 10 + random.nextInt(90);
         }
+        System.out.println("Insertion Sort");
         System.out.println("Unsorted Array : " + Arrays.toString(array));
         InsertionSort.performSort(array);
         System.out.println("Sorted Array : " + Arrays.toString(array));
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 10 + random.nextInt(90);
+        }
+
+        System.out.println("Merge Sort");
+        System.out.println("Unsorted Array : " + Arrays.toString(array));
+        MergeSort.mergeSort(array, 0, array.length - 1);
+        System.out.println("Sorted Array : " + Arrays.toString(array));
+        System.out.println();
+
     }
 }
